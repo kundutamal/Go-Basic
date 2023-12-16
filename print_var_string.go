6b64c1663f92ae337635c65b233b0d3f6241455e
kundutamal <kundu.tamal2022@gmail.com>
2023-12-16 21:20:30 +0530

    test

--- print_var_string.go
+++ print_var_string.go
@@ -7,5 +7,20 @@ func main() {
 	var name string = "Kodecloud"
 	var user string = "Tamal"
 
-	fmt.Print("welcome to ", name, ", ", user)
+	fmt.Print("welcome to ", name, ", ", user, "\n")
+
+	var name1 string = "Hari"
+	var grade int = 78
+	fmt.Printf("Hey! %v you have scored %d \n", name1, grade)
+
+	var firstname, lastname string = "tamal", "kundu"
+	fmt.Println(firstname)
+	fmt.Println(lastname)
+
+	var (
+		firstname1 string = "Tamal"
+		age        int    = 32
+	)
+	fmt.Println(firstname1)
+	fmt.Println(age)
 }
